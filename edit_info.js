7a0910b3b17ad0156d0a18bfdd7a39d40f28af7c
ziinxd <138343042+ziinxd@users.noreply.github.com>
2023-07-02 10:39:32 +0700

    Add files via upload

---
+++ edit_info.js
@@ -0,0 +1,27 @@
+const { modul } = require('./module');
+const edit_info = `Di Bawah`// ga usah di otak atik
+
+const urlpepe = `https://c.top4top.io/p_2722fgolo3.jpg`// url/link foto
+
+const namaowner = `♕𝐌𝐓𝐒 𝐒𝐓𝐎𝐑𝐄♕`// nama owner
+const noowner = `628989105685`// nomor owner
+
+const packname = `ayam`// nama pack sticker
+const author = `kokok`// nama author stiker
+
+const pending = `1000`// waktu tunggu broadcast
+/**EDIT WAKTU TUNGGU BROADCAST DI ATAS
+CONTOH :
+1000 = 1 DETIK
+2000 = 2 DETIK
+3000 = 3 DETIK
+DAN SETERUS NYA
+
+🚫TIDAK DI SARAN KAN UNTUK DI BAWAH 1 DETIK🚫
+KARENA AKAN MENYEBAB KAN WA TERBLOKIR
+
+BELI SCRIPT NO ENC, CHAT WHATSAPP DI BAWAH
+HARGA : Rp 50.000
+NOMOR WA : 628989105685
+**/
+module.exports = { edit_info,urlpepe,namaowner,noowner,packname,author,pending }
